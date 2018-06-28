63d63ba9872acb1a75883050012301f3ce6dc5e5
manager <manager@163.com>
2018-06-28 17:23:03 +0800

    静态化首页的手动脚本

---
+++ meiduo_mall/scripts/regenerate_index_html.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# coding=utf-8
+
+import sys
+sys.path.insert(0,'../')
+# sys.path.insert(0,'../meiduo_mall/apps')
+
+
+import os
+if not os.getenv('DJANGO_SETTINGS_MODULE'):
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'meiduo_mall.settings.dev'
+
+# 让django进行初始化设置
+import django
+django.setup()
+
+
+
+from contents.crons import generate_static_index_html
+if __name__ == '__main__':
+    generate_static_index_html()
